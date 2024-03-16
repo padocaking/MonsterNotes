5cf97c8250c7c77615ebcc8922d2baead183eb46
Lucas <lucascardoso0221@gmail.com>
2024-03-16 07:32:20 -0300

    Main monster info display finished and responsive

---
+++ script.js
@@ -0,0 +1,15 @@
+const navMenu = document.querySelector(".menu-nav");
+const navMonster = document.querySelector(".navMonster");
+const navBackground = document.querySelector(".nav-background");
+
+navMenu.addEventListener("click", () => {
+    navMonster.classList.add("open");
+    navBackground.classList.add("active");
+});
+
+navBackground.addEventListener("click", () => {
+    navMonster.classList.remove("open");
+    navBackground.classList.remove("active");
+});
+
+console.log(navMenu);
